dc74cf85fb1ea148deafe12d0272793e3623df74
Iryna Tiutiun <tiutiuniryna@gmail.com>
2021-11-19 01:03:57 +0300

    task2-changed the Main file and added the name in static text

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,15 @@
 import React from "react";
-import { View, StyleSheet } from 'react-native';
+import { View } from 'react-native';
 import Header from "./Header";
 import { TITLE } from '../constants/Constants';
 import Main from "./Main";
 
 export default function Home() {
     return (
-        <View style={styles.container}>
+        <View>
             <Header title={TITLE} />
             <Main />
         </View>
     );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,27 +1,37 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TextInput } from 'react-native';
-
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
 
 const Main = () => {
 
     const [name, setName] = useState('');
-    const [textBody, setTextBody] = useState('');
+    const [textBody, setTextBody] = useState('friend');
 
     const changeName = (event) => {
         setName(event);
-        console.log(event);
+    }
+
+    const addName = () => {
+        if (name === '') {
+            return alert('The field cannot be empty, please enter your name');
+        }
+        setTextBody(name);
+        setName('');
     }
 
     return (
         <View style={styles.container}>
             <View style={styles.wrapper}>
                 <TextInput
+                    style={styles.input}
                     onChangeText={changeName}
                     placeholder={'write your name'}
                     value={name}
                 />
+                <Button title={'ok'} onPress={addName} />
             </View>
-            <Text>Welcome  {textBody} !</Text>
+            <View >
+                <Text style={styles.text} >Welcome  {textBody} !</Text>
+            </View>
         </View>
     );
 };
@@ -33,8 +43,18 @@ const styles = StyleSheet.create({
         alignItems: 'center'
     },
     wrapper: {
+        flexDirection: 'row',
         borderBottomWidth: 1,
-        marginTop: 12
+        marginTop: 12,
+
+    },
+    input: {
+        textAlign: 'center'
+
+    },
+    text: {
+        marginTop: 30,
+        fontSize: 25
     }
 });
 export default Main;
