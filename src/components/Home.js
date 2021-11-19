4597f68abd18bfc197a80ef365b3b7b05af32bf8
Iryna Tiutiun <tiutiuniryna@gmail.com>
2021-11-19 09:40:07 +0300

    task2-added Footer file and change Home file

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,12 +3,20 @@ import { View, StyleSheet } from 'react-native';
 import Header from "./Header";
 import { TITLE } from '../constants/Constants';
 import Main from "./Main";
+import Footer from "./Footer"
 
 export default function Home() {
     return (
         <View style={styles.container}>
-            <Header title={TITLE} />
-            <Main />
+            <View style={styles.header}>
+                <Header title={TITLE} />
+            </View>
+            <View style={styles.main}>
+                <Main />
+            </View>
+            <View style={styles.footer}>
+                <Footer />
+            </View>
         </View>
     );
 }
@@ -16,5 +24,14 @@ export default function Home() {
 const styles = StyleSheet.create({
     container: {
         flex: 1
+    },
+    header: {
+        flex: .1
+    },
+    main: {
+        flex: .8
+    },
+    footer: {
+        flex: .1
     }
 })
